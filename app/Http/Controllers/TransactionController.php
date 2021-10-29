924929e992e9ce9915bf6f1a8a0096e21b6f1f35
Cassiano S <cassiano@scheidemantel.com.br>
2021-10-29 09:18:06 -0300

    fix: syntax error tokens and return old return use

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Response;
 
 use App\Models\Transaction;
 
--- app/Http/Middleware/JwtMIddleware.php
+++ app/Http/Middleware/JwtMIddleware.php
@@ -23,11 +23,11 @@ class JwtMiddleware extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
             if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
-                return response()->json(['message' => 'Token is Invalid'], 401);
+                return response()->json(['message' => ['Token is Invalid']], 401);
             } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
-                return response()->json(['message' => 'Token is Expired'], 401);
+                return response()->json(['message' => ['Token is Expired']], 401);
             } else {
-                return response()->json(['message' => 'Authorization Token not found'], 401);
+                return response()->json(['message' => ['Authorization Token not found']], 401);
             }
         }
         return $next($request);
